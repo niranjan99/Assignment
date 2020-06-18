c811597f088f3b599bf7802b56f9d751a177fa54
Niranjan <vaddepalli.niranjan@gmail.com>
2020-06-18 18:57:21 +0530

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Niranjan on 18/06/20.
+//  Copyright © 2020 Niranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
