64fcf5f55a59ed2a8a4e316ce4d49d4b264d8a1a
Niranjan <vaddepalli.niranjan@gmail.com>
2020-06-26 15:27:47 +0530

    1. ViewController initialised Programmatically 2. Indentation has been corrected 3. Tableview Auto dimension is working. but if image view height is dynamic we need to set the bottom to bottom layout. right now we have set it to nil as it constant height. Now we are checking the description only as dynamic height. so based on description height it table view auto dimension is working. 4. Pull to refresh is calling API and reloading the data, Kindly check by putting on breakpoints.

--- Assignment/View/ViewController.swift
+++ Assignment/View/ViewController.swift
@@ -10,77 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var viewModel = FactsViewModel()
-    let contactsTableView = UITableView()
-    var refreshControl = UIRefreshControl()
-    var activityView = UIActivityIndicatorView()
-     override func viewDidLoad() {
-        super.viewDidLoad()
-        setupView()
-    }
-    func setupView(){
-               initTableView()
-               showActivityIndicator()
-               viewModel.loadData()
-               reloadTableView()
-               refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
-               refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
-               contactsTableView.addSubview(refreshControl)
-    }
-    @objc func refresh(_ sender: AnyObject) {
-          viewModel.loadData()
-       }
-    func reloadTableView() {
-           viewModel.reloadList = { [weak self] ()  in
-               DispatchQueue.main.async {
-                self?.hideActivityIndicator()
-                self?.refreshControl.endRefreshing()
-                self?.contactsTableView.reloadData()
-                self?.title = self?.viewModel.feedData?.title
-               }
-           }
-       }
-
-    func initTableView(){
-          view.addSubview(contactsTableView)
-
-          contactsTableView.translatesAutoresizingMaskIntoConstraints = false
-          contactsTableView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
-          contactsTableView.leftAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leftAnchor).isActive = true
-          contactsTableView.rightAnchor.constraint(equalTo:view.safeAreaLayoutGuide.rightAnchor).isActive = true
-          contactsTableView.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-          contactsTableView.dataSource = self
-          contactsTableView.delegate = self
-          contactsTableView.register(DataTableViewCell.self, forCellReuseIdentifier: "DataTableCell")
-
-    }
-    func showActivityIndicator() {
-        activityView = UIActivityIndicatorView(style: .large)
-                activityView.center = view.center
-                activityView.hidesWhenStopped = true
-                activityView.startAnimating()
-         contactsTableView.addSubview(activityView)
-    }
-
-    func hideActivityIndicator(){
-            activityView.stopAnimating()
-    }
+  let viewModel = FactsViewModel()
+  let contactsTableView = UITableView()
+  var refreshControl = UIRefreshControl()
+  var activityView = UIActivityIndicatorView()
+  
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .white
+      setupView()
    }
 
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return UITableView.automaticDimension
-        }
-}
-
-extension ViewController: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return viewModel.feedData?.rows?.count ?? 0
-    }
+  func setupView(){
+      initTableView()
+      showActivityIndicator()
+      viewModel.loadData()
+      reloadTableView()
+      refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+      contactsTableView.addSubview(refreshControl)
+  }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DataTableCell", for: indexPath) as! DataTableViewCell
-        cell.setData = viewModel.feedData?.rows?[indexPath.row]
-        return cell
+ @objc func refreshData(_ sender: AnyObject) {
+       viewModel.loadData()
+  }
+    
+ func reloadTableView() {
+        viewModel.reloadList = { [weak self] ()  in
+            DispatchQueue.main.async {
+             self?.hideActivityIndicator()
+             self?.refreshControl.endRefreshing()
+             self?.contactsTableView.reloadData()
+             self?.title = self?.viewModel.feedData?.title
+                self?.viewDidLayoutSubviews()
+            }
+        }
     }
+
+ func initTableView(){
+     view.addSubview(contactsTableView)
+    contactsTableView.anchor(top: view.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0, enableInsets: false)
+     contactsTableView.register(DataTableViewCell.self, forCellReuseIdentifier: "DataTableCell")
+     contactsTableView.delegate = viewModel
+     contactsTableView.dataSource = viewModel
+    // contactsTableView.estimatedRowHeight = 150
+     contactsTableView.rowHeight = UITableView.automaticDimension
+     contactsTableView.translatesAutoresizingMaskIntoConstraints = false
+ }
+  
+ func showActivityIndicator() {
+     activityView = UIActivityIndicatorView(style: .large)
+     activityView.center = view.center
+     activityView.hidesWhenStopped = true
+     activityView.startAnimating()
+     contactsTableView.addSubview(activityView)
+ }
+  
+ func hideActivityIndicator(){
+      activityView.stopAnimating()
+ }
 }
